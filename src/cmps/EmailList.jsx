a47f334155fc078e5686c01bbfd8a2806055fd03
yuvalpiwix <yuvalpi@wix.com>
2024-02-28 19:27:42 +0200

    email lisy icons

--- src/cmps/EmailList.jsx
+++ src/cmps/EmailList.jsx
@@ -6,7 +6,22 @@ export function EmailList({ emails }) {
       {emails.map((email) => {
         return (
           <li key={email.id}>
+            <div className="email-selection-btns">
+              <div className="email-drag-handle"></div>
+              <div className="email-checbox"></div>
+              <div
+                className={`email-star ${email.isStarred ? "starred" : ""}`}
+              ></div>
+            </div>
             <EmailPreview email={email} />
+            <div className="email-actions">
+              <div className="email-archive"></div>
+              <div className="email-delete"></div>
+              <div
+                className={`email-read ${email.isRead ? "readed" : ""}`}
+              ></div>
+              <div className="email-snooze"></div>
+            </div>
           </li>
         );
       })}
