fc58c5585528f10df05c3d57dead2111323dbd10
yuvalpiwix <yuvalpi@wix.com>
2024-03-09 10:57:36 +0200

    compose view state

--- src/cmps/EmailCompose.jsx
+++ src/cmps/EmailCompose.jsx
@@ -6,6 +6,8 @@ export function EmailCompose({ params, onAddEmail }) {
   const navigate = useNavigate();
 
   const [email, setEmail] = useState(emailService.getDefaultEmail());
+  //minimized, normal, fullscreen
+  const [viewState, setViewState] = useState("normal");
 
   function handleChange(ev) {
     let { name: field, value, type } = ev.target;
@@ -31,48 +33,62 @@ export function EmailCompose({ params, onAddEmail }) {
   }
 
   return (
-    <div className="email-compose">
-      <div className="compose-header">
-        <div className="title">New Messege</div>
-        <div className="actions">
-          <span className="icon minimize"></span>
-          <span className="icon openClose-fullScren"></span>
-          <Link to={`/email/${params.folder}`}>
-            <span className="icon close"></span>
-          </Link>
+    <div className={`email-compose-wrapper ${viewState}`}>
+      <div className="email-compose">
+        <div className="compose-header">
+          <div className="title">New Messege</div>
+          <div className="actions">
+            <span
+              onClick={() => setViewState("minimized")}
+              className="icon minimize"
+            ></span>
+            <span
+              onClick={() =>
+                setViewState((prevView) =>
+                  prevView == "normal"
+                    ? (prevView = "fullscreen")
+                    : (prevView = "normal")
+                )
+              }
+              className="icon openClose-fullScren"
+            ></span>
+            <Link to={`/email/${params.folder}`}>
+              <span className="icon close"></span>
+            </Link>
+          </div>
         </div>
+        <form onSubmit={onSaveEmail}>
+          <label htmlFor="to">
+            <input
+              type="email"
+              id="to"
+              placeholder="to"
+              name="to"
+              value={email.to}
+              onChange={handleChange}
+            />
+          </label>
+          <label htmlFor="subject">
+            <input
+              type="text"
+              id="subject"
+              placeholder="subject"
+              name="subject"
+              value={email.subject}
+              onChange={handleChange}
+            />
+          </label>
+          <textarea
+            name="body"
+            id=""
+            cols="30"
+            rows="10"
+            value={email.body}
+            onChange={handleChange}
+          ></textarea>
+          <button>send</button>
+        </form>
       </div>
-      <form onSubmit={onSaveEmail}>
-        <label htmlFor="to">
-          <input
-            type="email"
-            id="to"
-            placeholder="to"
-            name="to"
-            value={email.to}
-            onChange={handleChange}
-          />
-        </label>
-        <label htmlFor="subject">
-          <input
-            type="text"
-            id="subject"
-            placeholder="subject"
-            name="subject"
-            value={email.subject}
-            onChange={handleChange}
-          />
-        </label>
-        <textarea
-          name="body"
-          id=""
-          cols="30"
-          rows="10"
-          value={email.body}
-          onChange={handleChange}
-        ></textarea>
-        <button>send</button>
-      </form>
     </div>
   );
 }
